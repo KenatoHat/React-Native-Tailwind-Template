059f76f01d1b540bc80cb4826c6eeae56d0d07df
kenathat <kenatohat@gmail.com>
2024-07-17 09:38:15 +0300

    --Template

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import "./global.css"
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-teal-500 ">
-      <Text className="text-3xl text-white font-bold ">Hello World</Text>
+      <Text className="text-3xl text-black font-bold ">Hello Ken!</Text>
       <StatusBar style="auto" />
     </View>
   );
